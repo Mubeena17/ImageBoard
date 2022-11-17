e711852fe7313914fbc266704a26bee9c3bfdbdc
mubeena <mubeenahamza@gmail.com>
2022-11-17 16:48:00 +0100

    fetch images

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,47 @@
-import * as Vue from './vue.js';
+import * as Vue from "./vue.js";
+
+Vue.createApp({
+    data() {
+        return {
+            headline: "My Vue App",
+            images: [],
+        };
+    },
+    mounted() {
+        fetch("/images")
+            .then((res) => {
+                return res.json();
+            })
+            .then((images) => {
+                console.log(images);
+                this.images = images;
+            });
+    },
+}).mount("#main");
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             headline: "My fancy image board",
+//             headlineCss: "headlineClass",
+//             cities: [],
+//             firstName: "",
+//             count: 0,
+//         };
+//     },
+//     methods: {
+//         updateName: function (e) {
+//             this.firstName = "Sven";
+//         },
+//         increaseCount: function () {
+//             this.count++;
+//         },
+//     },
+//     mounted() {
+//         fetch("/cities")
+//             .then((res) => res.json())
+//             .then((cities) => {
+//                 this.cities = cities;
+//             });
+//     },
+// }).mount("#main");
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const { PORT = 8080 } = process.env;
+const { getImage } = require("./db");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.get("/images", (req, res) => {
+    getImage().then((result) => {
+        return res.send(result);
+    });
+});
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
