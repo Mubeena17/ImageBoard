8c4b832f61903d5417e2e5e5f7c4c1d37f48a0f6
mubeena <mubeenahamza@gmail.com>
2022-11-18 15:08:43 +0100

    upload image to server

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,28 @@ Vue.createApp({
             headline: "My Vue App",
             images: [],
             cardCSS: "data-card",
+            message: "",
+            photo: "",
         };
+    },
+    methods: {
+        uploadImage: function (e) {
+            e.preventDefault();
+            console.log("clicked");
+            const myFileInput = document.querySelector("input[type='file']");
+            const photo = myFileInput.files[0];
+            const formData = new FormData();
+            formData.append("photo", photo);
+            fetch("/image", {
+                method: "POST",
+                body: formData,
+            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((result) => (this.message = result.message))
+                .catch((err) => console.log(err));
+        },
     },
     mounted() {
         fetch("/images")
--- server.js
+++ server.js
@@ -4,14 +4,34 @@ const app = express();
 require("dotenv").config();
 const { PORT = 8080 } = process.env;
 const { getImage } = require("./db");
+const { uploader } = require("./middleware");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploads")));
+
 app.use(express.json());
 
 app.get("/images", (req, res) => {
     getImage().then((result) => {
         return res.send(result);
     });
+});
+
+app.post("/image", uploader.single("photo"), (req, res) => {
+    console.log("image in server");
+    if (req.file) {
+        console.log("file is", req.file);
+        res.json({
+            success: true,
+            message: "File upload successful",
+            file: `/${req.file.filename}`,
+        });
+    } else {
+        res.json({
+            success: false,
+            message: "File upload failed",
+        });
+    }
 });
 
 app.get("*", (req, res) => {
